ad020ab7add70ac876c548b7800728a0da9fd37e
VijaychandarG <vijayrko1997@gmail.com>
2021-07-24 18:17:41 +0530

    Modified app.py, moved the db initialization block outside

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ from resources.store import Store, StoreList
 import datetime
 from db import db
 
+db.init_app(app)
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -36,5 +38,4 @@ api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
 
 if __name__ == '__main__':
-	db.init_app(app)
 	app.run(port=5000, debug = True)
